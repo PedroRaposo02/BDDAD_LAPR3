50c364c99c5794fbc2c510bb08dff74986e3d7e8
reel23 <1201463@isep.ipp.pt>
2023-10-23 17:08:12 +0100

    update modeloFisico, Criaçao de model

--- csvToSql.py
+++ csvToSql.py
@@ -1,3 +1,5 @@
 # Script solely used to convert legacy csv with the specified format to database
 
+#to import "Culturas" table, "Parcelas" must be created first (its not in Legacy Data).
+
 import csv
