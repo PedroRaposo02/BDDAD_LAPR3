e86ce76d049f41be15774fa6f4ee0b54a4b924de
Pedro Raposo <peraposo@hotmail.com>
2023-11-26 21:52:22 +0000

    US20

--- sprint2/US20.sql
+++ sprint2/US20.sql
@@ -1,5 +1,4 @@
 CREATE OR REPLACE FUNCTION LISTA_TOTAL_REGA_MENSAL (
-    V_PARCELA IN VARCHAR2,
     V_DATA_INICIAL IN DATE,
     V_DATA_FINAL IN DATE
 ) RETURN SYS_REFCURSOR AS
@@ -7,21 +6,23 @@ CREATE OR REPLACE FUNCTION LISTA_TOTAL_REGA_MENSAL (
 BEGIN
     OPEN LISTA FOR
         SELECT
+            PA.DESIGNACAO              AS PARCELA,
             TO_CHAR(O.DATA, 'YYYY-MM') AS MES,
-            SUM(O.QUANTIDADE)          AS TOTAL_REGA
+            SUM(O.QUANTIDADE) * 60 AS TOTAL_REGA
         FROM
-            OPERACAO O
+            OPERACAO         O
             LEFT JOIN CULTURA C
             ON O.CULTURA_ID = C.ID
             LEFT JOIN PARCELA_AGRICOLA PA
             ON C.PARCELA_ID = PA.ID
         WHERE
-            PA.DESIGNACAO = V_PARCELA
-            AND O.TIPO_OPERACAO = 'Rega'
+            O.TIPO_OPERACAO = 'Rega'
             AND O.DATA BETWEEN V_DATA_INICIAL AND V_DATA_FINAL
         GROUP BY
+            PA.DESIGNACAO,
             TO_CHAR(O.DATA, 'YYYY-MM')
         ORDER BY
+            PA.DESIGNACAO,
             TO_CHAR(O.DATA, 'YYYY-MM');
     RETURN LISTA;
 END;
@@ -29,22 +30,25 @@ END;
 
 /*test cursor*/
 DECLARE
-    LISTA         SYS_REFCURSOR;
-    MES           VARCHAR2(20);
-    TOTAL_REGA    FLOAT;
-    V_PARCELA     VARCHAR2(20) := 'Campo Grande';
-    DATA_INICIAL  DATE := TO_DATE('2015-09-01', 'YYYY-MM-DD');
-    DATA_FINAL    DATE := TO_DATE('2023-09-30', 'YYYY-MM-DD');
+    LISTA        SYS_REFCURSOR;
+    PARCELA      VARCHAR2(20);
+    MES          VARCHAR2(20);
+    TOTAL_REGA   FLOAT;
+    DATA_INICIAL DATE := TO_DATE('2010-06-01', 'YYYY-MM-DD');
+    DATA_FINAL   DATE := TO_DATE('2023-11-06', 'YYYY-MM-DD');
 BEGIN
-    LISTA := LISTA_TOTAL_REGA_MENSAL (V_PARCELA, DATA_INICIAL, DATA_FINAL);
-    dbms_output.put_line ('MES | TOTAL_REGA');
+    LISTA := LISTA_TOTAL_REGA_MENSAL (DATA_INICIAL, DATA_FINAL);
+    DBMS_OUTPUT.PUT_LINE ('PARCELA - MES - TOTAL_REGA');
     DBMS_OUTPUT.PUT_LINE ('----------------------------------------------------------------------');
     LOOP
-        FETCH LISTA INTO MES, TOTAL_REGA;
+        FETCH LISTA INTO PARCELA, MES, TOTAL_REGA;
         EXIT WHEN LISTA%NOTFOUND;
-        DBMS_OUTPUT.PUT_LINE (MES
-                              || ' | '
-                              || TOTAL_REGA);
+        DBMS_OUTPUT.PUT_LINE (PARCELA
+                              || ' - '
+                              ||MES
+                              || ' - '
+                              || TOTAL_REGA
+                              || ' min');
     END LOOP;
 
     CLOSE LISTA;
